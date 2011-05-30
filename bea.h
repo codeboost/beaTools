158ca242b3d708c59c0763efb95a3fde7235d176
Florin <florin@libertv.ro>
2011-05-30 17:22:22 +0200

    fixed the delete void* p

--- bea.h
+++ bea.h
@@ -384,6 +384,35 @@ namespace bea{
 		}
 	};
 
+	template<> struct Convert<unsigned long> {
+		static bool Is(v8::Handle<v8::Value> v) {
+			return bea::Convert<unsigned int>::Is(v);
+		}
+
+		static unsigned long FromJS(v8::Handle<v8::Value> v, int nArg) {
+			return (unsigned long)bea::Convert<unsigned int>::FromJS(v, nArg);
+		}
+
+		static v8::Handle<v8::Value> ToJS(unsigned long const& v) {
+			return bea::Convert<unsigned int>::ToJS(v);
+		}
+
+	};
+
+	template<> struct Convert<long> {
+		static bool Is(v8::Handle<v8::Value> v) {
+			return bea::Convert<int>::Is(v);
+		}
+
+		static unsigned long FromJS(v8::Handle<v8::Value> v, int nArg) {
+			return (long)bea::Convert<int>::FromJS(v, nArg);
+		}
+
+		static v8::Handle<v8::Value> ToJS(long const& v) {
+			return bea::Convert<int>::ToJS((int const&)v);
+		}
+
+	};
 
 
 	typedef void (*reportExceptionCb)(v8::TryCatch&);
@@ -547,7 +576,7 @@ namespace bea{
 				if (_this->m_destructor)
 					_this->m_destructor(value);
 				else{
-					delete p;
+					delete static_cast<T*>(p);
 				}
 
 			}
